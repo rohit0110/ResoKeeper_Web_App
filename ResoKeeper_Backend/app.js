4240113c7b42beb1eda6483fbf8e72afe32d4092
rohit0110 <rohit7rathore15@gmail.com>
2021-01-25 23:39:45 +0530

    Backend Connection

--- ResoKeeper_Backend/app.js
+++ ResoKeeper_Backend/app.js
@@ -7,7 +7,29 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+//what i have written
+const mysql=require('mysql');
+
+//CREATE CONNECTION
+const db=mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database: 'ResoKeeper_Database'
+});
+
+//CONNECT
+db.connect((err)=>{
+  if(err){
+    throw err;
+  }
+  console.log("MySQL connected");
+});
+
+const app=express();
+app.listen('3000',()=>{
+    console.log('Server has started');
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,7 +47,7 @@ app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+});ssss
 
 // error handler
 app.use(function(err, req, res, next) {
